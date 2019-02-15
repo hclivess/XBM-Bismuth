9d03dca3bbabeb1c8e270e477a643ee8a8546289
raetsch <32565282+raetsch@users.noreply.github.com>
2019-02-15 11:23:12 +0100

    balance_nogui updated
    using API and WalletServer infrastructure, so independent from local node, using wallet.der

--- balance_nogui.py
+++ balance_nogui.py
@@ -1,24 +1,83 @@
-import essentials, options, connections, socks
+import connections, socks
+from keys import read
+from sys import exit
+import json
+import requests
 
-config = options.Get()
-config.read()
-node_ip_conf = config.node_ip_conf
-port = config.port
+port = 8150
 
-key, public_key_readable, private_key_readable, encrypted, unlocked, public_key_hashed, address = essentials.keys_load("privkey.der", "pubkey.der")
+def convert_ip_port(ip, some_port):
+    """
+    Get ip and port, but extract port from ip if ip was as ip:port
+    :param ip:
+    :param some_port: default port
+    :return: (ip, port)
+    """
+    if ':' in ip:
+        ip, some_port = ip.split(':')
+    return ip, some_port
+    
+    
+key, public_key_readable, private_key_readable, public_key_hashed, address = read()
 
-s = socks.socksocket()
-s.settimeout(10)
-s.connect((node_ip_conf, int(port)))
+wallets = []
 
-connections.send (s, "balanceget", 10)
-connections.send (s, address, 10)
-balanceget_result = connections.receive (s, 10)
+try:
+    rep = requests.get("http://api.bismuth.live/servers/wallet/legacy.json")
+    if rep.status_code == 200:
+        wallets = rep.json()
+        #print(wallets)
+except Exception as e:
+    print("Error {} getting Server list from API, using lwbench instead".format(e))
+
+
+if wallets:
+    # We have a server list, order by load
+    sorted_wallets = sorted([wallet for wallet in wallets if wallet['active']], key=lambda k: (k['clients']+1)/(k['total_slots']+2))
+    #print(sorted_wallets)
+    """
+    # try to connect in sequence, keep the first one ok.
+    for wallet in sorted_wallets:
+        print(wallet)
+        ipport = "{}:{}".format(wallet['ip'], wallet['port'])
+        print(ipport)
+        if lwbench.connectible(ipport):
+            return [ipport]
+    """
+    if sorted_wallets:
+        light_ip = ["{}:{}".format(wallet['ip'], wallet['port']) for wallet in sorted_wallets]
+
+else:
+    exit("No Server reachable, try again in few minutes or ask for help in #support in https://discordapp.com/channels/348020833194868751/392572196855480320")
+     
+ 
+keep_trying = True
+while keep_trying:
+    for ip in light_ip:
+        try:
+            ip, local_port = convert_ip_port(ip, port)
+            s = socks.socksocket()
+            s.settimeout(10)
+            s.connect((ip, int(local_port)))
+            connections.send (s, "balanceget", 10)
+            connections.send (s, address, 10)
+            balanceget_result = connections.receive (s, 10)
+            keep_trying = False
+            break
+        except Exception as e:
+            print("Status: Cannot connect to {}:{}".format(ip, local_port))
+            time.sleep(1)
+
+
+
+
+
 print ("Address balance: {}".format (balanceget_result[0]))
 print ("Address credit: {}".format (balanceget_result[1]))
 print ("Address debit: {}".format (balanceget_result[2]))
 print ("Address fees: {}".format (balanceget_result[3]))
 print ("Address rewards: {}".format (balanceget_result[4]))
 print ("Address balance without mempool: {}".format (balanceget_result[5]))
-# get balance
+exit("Done!")
+
 
